726d117bc049003680160d85e0db17d5f87cc537
Nathan Sadler <nssadler@my.waketech.edu>
2021-06-07 18:44:40 -0400

    refactored code and removed card_hand

--- lib/player.rb
+++ lib/player.rb
@@ -2,18 +2,22 @@ require_relative('playing_card')
 require_relative('card_hand')
 
 class Player
-  attr_accessor :hand, :name
-  attr_reader  :id, :client
+  attr_accessor :name
+  attr_reader  :id, :client, :cards
 
   def initialize(id = 0, name = nil)
-    @hand = CardHand.new
+    @cards = []
     @name = name
     @id = id
     @client = nil
   end
 
-  def add_card_to_hand(card)
-    hand.add_card(card)
+  def add_card_to_hand(to_add)
+    if to_add.is_a?(Array)
+      set_cards(cards.concat(to_add))
+    else
+      set_cards(cards.push(to_add))
+    end
   end
 
   def get_cards_from_player(rank, player)
@@ -26,6 +30,20 @@ class Player
     @client = client
   end
 
+  def set_cards(card_list)
+    @cards = card_list
+  end
+
+  def has_card_with_rank?(rank)
+    selected_cards = cards.select {|card| card.rank == rank}
+    selected_cards.length > 0
+  end
+
+  def remove_cards_with_rank(rank)
+    cards_with_rank = cards.select {|card| card.rank == rank}
+    set_cards(cards.difference(cards_with_rank))
+    cards_with_rank
+  end
 
   # Be sure to override hash later!
   def ==(other_player)
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -26,6 +26,15 @@ describe('Player') do
     end
   end
 
+  describe('.set_cards') do
+    it('sets the cards of the player') do
+      card_a = PlayingCard.new("4", "S")
+      card_b = PlayingCard.new("7", "D")
+      player.set_cards([card_a, card_b])
+      expect(player.cards).to(eq([card_a, card_b]))
+    end
+  end
+
   describe('==') do
     it('is true if both players have the same ID') do
       expect(Player.new(0, "John Doe")).to(eq(Player.new(0, "John Doe")))
@@ -38,14 +47,42 @@ describe('Player') do
     end
   end
 
-  context('.add_card_to_hand') do
-    it("adds a card to a player's hand") do
-      player.add_card_to_hand(PlayingCard.new("4", "C"))
-      expect(player.hand.has_card_with_rank?("4")).to(eq(true))
+  describe('.has_card_with_rank?') do
+    it('returns true if it has a card with a given rank') do
+      player.add_card_to_hand(PlayingCard.new('3', 'C'))
+      expect(player.has_card_with_rank?('3')).to(eq(true))
+    end
+    it('returns false if it does not have a card with a given rank') do
+      player.add_card_to_hand(PlayingCard.new('4', 'D'))
+      expect(player.has_card_with_rank?('K')).to(eq(false))
     end
   end
 
-  # TODO: finish after implementing add_card_to_hand
+  context('.add_card_to_hand') do
+    it("adds a card to a player's hand") do
+      player.add_card_to_hand(PlayingCard.new("4", "C"))
+      expect(player.cards).to(eq([PlayingCard.new("4", "C")]))
+    end
+  end
+
+  describe('.remove_cards_with_rank') do
+    it('removes all cards with a given rank from the hand and returns them') do
+      card_a = PlayingCard.new("4", "S")
+      card_b = PlayingCard.new("7", "D")
+      card_c = PlayingCard.new("7", "S")
+      player.add_card_to_hand([card_a, card_b, card_c])
+      expect(player.remove_cards_with_rank("7")).to(eq([card_b, card_c]))
+      expect(player.cards).to(eq([card_a]))
+    end
+    it('returns an empty array if there are no cards with the given rank in the hand') do
+      card_a = PlayingCard.new("4", "S")
+      card_b = PlayingCard.new("5", "S")
+      player.add_card_to_hand([card_a, card_b])
+      expect(player.remove_cards_with_rank("6")).to(eq([]))
+    end
+  end
+
+  # TODO: finish this
   context('.get_cards_from_player') do
     it("removes cards with a specific rank from a specific player's and and adds"+
     " them to its own hand") do
